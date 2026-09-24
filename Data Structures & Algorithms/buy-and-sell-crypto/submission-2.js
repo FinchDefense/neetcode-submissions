94506c8b987c8b2ab96bfc0c64bb1a0ba1e9327a
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-09-23 20:44:55 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxProfit = 0;
+        let minPrice = Infinity; 
+
+        for (let i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i]; 
+            } else {
+                const currentProfit = prices[i] - minPrice;
+                if (currentProfit > maxProfit) {
+                    maxProfit = currentProfit; 
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+}
